a3b3854d173366b6059767e6348d130846816e5b
aguevara <alex@digitalflashcards.com>
2023-11-22 07:26:08 -0500

    minimize iterations

--- maximize-profit.js
+++ maximize-profit.js
@@ -6,18 +6,21 @@ prices.sort((x,y) => parseFloat(x.Price) - parseFloat(y.Price));
 let bestTrade = {
 	"BuyDate": new Date(),
 	"BuyPrice": 0.0,
+	"BuyIndex": prices.length - 1,
 	"SellDate": new Date(),
 	"SellPrice": 0.0,
 	"Profit": 0.0,
-	"Rate": 0.0,
-	"BuyIndex": 0
+	"Rate": 0.0
 };
 
 for(let i=prices.length-1; i>-1; i--)
-for(let j=0; j < i; j++) {
+for(let j=0;
+	j < i && j < bestTrade.BuyIndex; // profit must be smaller for any greater j
+	j++) {
 	const trade = {
 		"BuyDate": new Date(prices[j].Date),
 		"BuyPrice": prices[j].Price,
+		"BuyIndex": j,
 		"SellDate": new Date(prices[i].Date),
 		"SellPrice": prices[i].Price,
 		"Profit": prices[i].Price - prices[j].Price,
@@ -26,6 +29,7 @@ for(let j=0; j < i; j++) {
 	if(trade.BuyDate < trade.SellDate)
 	if(trade.Profit > bestTrade.Profit){
 		bestTrade = trade;
+		break; // profit must be smaller for any greater j
 	}
 };
 
